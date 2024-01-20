7b501649237e0abd233e0b20ceb55d244ad27dc7
Seclud <82933148+Seclud@users.noreply.github.com>
2024-01-21 00:16:29 +0500

    Add necessary models

--- DjangoApp/models.py
+++ DjangoApp/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 
-class TodoItem(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+# class TodoItem(models.Model):
+#     title = models.CharField(max_length=200)
+#     completed = models.BooleanField(default=False)
+
+
+class Skill(models.Model):
+    image = models.ImageField(upload_to='skills/')
+
+
+class Demand(models.Model):
+    image = models.ImageField(upload_to='demand/')
+
+
+class Geography(models.Model):
+    image = models.ImageField(upload_to='geography/')
