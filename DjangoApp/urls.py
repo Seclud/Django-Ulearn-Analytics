332f9d2475c43ee8300469027f257b68d91a1160
Seclud <82933148+Seclud@users.noreply.github.com>
2024-01-21 00:15:15 +0500

    Removed unnecessary urls, Add static media

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('todos/', views.todos, name="Todos"),
+    # path('todos/', views.todos, name="Todos"),
     path('skills/', views.skills, name='skills'),
     path('last_vacancies/', views.last_vacancies, name='last_vacancies'),
     path('demand/', views.demand, name='demand'),
     path('geogrpahy/', views.geography, name='geography'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
